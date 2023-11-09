e36e3aabbbc01a2bef0aef6fa94dd1ef03e6a271
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-11-10 01:00:06 +0200

    додає виконання 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtnEl = document.querySelector("button[data-start]");
+const stopBtnEl = document.querySelector("button[data-stop]");
+const bodyEl = document.querySelector("body");
 
+let timerId = null;
+stopBtnEl.setAttribute("disabled", "disabled");
+
+function getRandomHexColor() {
+return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+startBtnEl.addEventListener("click", onStartBtnClick);
+stopBtnEl.addEventListener("click", onStopBtnClick);
+
+function onStartBtnClick() {
+  startBtnEl.setAttribute("disabled", "disabled");
+  stopBtnEl.removeAttribute("disabled", "disabled");
+  timerId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+  startBtnEl.removeAttribute("disabled", "disabled");
+  stopBtnEl.setAttribute("disabled", "disabled");
+}
