df1a6c2261e7d613cc3330fad33441f0ebf83474
Dmitriy Dudnik <84873427+Wed88@users.noreply.github.com>
2023-11-11 19:46:24 +0200

    додає виконання 3 завдання

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const { delay, step, amount } = event.currentTarget;
+
+  let delayPromise = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i++) {
+    createPromise(i, delayPromise).then(onFulfilled).catch(onRejected);
+    delayPromise += Number(step.value);
   }
-}
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+};
+
+function onFulfilled(result) {
+  iziToast.success({message: `${result}`});
+};
+
+function onRejected(error) {
+  iziToast.error({message: `${error}`});
+};
